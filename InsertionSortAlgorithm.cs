5a9ac4c25e3b6f8edd68d11fd0eb8cc75b837b29
Safi <mdsafi729@gmail.com>
2023-05-06 13:37:41 +0530

    [shamsuddin]Fix: Adding Insertion File

---
+++ InsertionSortAlgorithm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class InsertionSort
+{
+    static void Main()
+    {
+        int[] arr = { 5, 2, 8, 4, 7, 1, 3, 6 };
+
+        Console.WriteLine("Original array:");
+        foreach (int i in arr)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine("\nSorted array:");
+        InsertionSortAlgorithm(arr);
+        foreach (int i in arr)
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    static void InsertionSortAlgorithm(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+}
