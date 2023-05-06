b2bc7e79b7d13d14b519eb5d40a2baa002bf799b
Safi <mdsafi729@gmail.com>
2023-05-06 13:41:48 +0530

    [shamsuddin]Fix: Adding Sort File

---
+++ SortAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DataStructureAlgorithmExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of integers separated by spaces:");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            int[] array = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                array[i] = int.Parse(numbers[i]);
+            }
+
+            Array.Sort(array);
+
+            Console.WriteLine("Sorted array:");
+            foreach (int number in array)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
